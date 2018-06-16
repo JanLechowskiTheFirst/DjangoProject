122859cc29103296630612ae758ca4bde9f452a0
Jan <youneedtominemoreminerals@gmail.com>
2018-06-16 09:56:42 +0200

    init

---
+++ git01/bin/django-admin.py
@@ -0,0 +1,6 @@
+#!/home/j/env01/git01/bin/python
+# EASY-INSTALL-DEV-SCRIPT: 'Django==2.2.dev20180615181808','django-admin.py'
+__requires__ = 'Django==2.2.dev20180615181808'
+__import__('pkg_resources').require('Django==2.2.dev20180615181808')
+__file__ = '/home/j/env01/git01/django-dev/django/bin/django-admin.py'
+exec(compile(open(__file__).read(), __file__, 'exec'))
---
+++ git01/django01/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/j/env01/git01/django01/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
